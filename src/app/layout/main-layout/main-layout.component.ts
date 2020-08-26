e09b2b37a4db09a74db63de8a9494ed2749a4fb0
Sergio Carrasco Álvarez <contact@sergiocarrasco.es>
2020-08-26 18:05:33 +0200

    Overlay to control when clicking outside of the sidebar

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { animations } from 'src/app/core/animations/animations';
+import { MainLayoutService } from './main-layout.service';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.scss']
+  styleUrls: ['./main-layout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [animations]
 })
 export class MainLayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public mainLayoutService: MainLayoutService
+  ) { }
 
   ngOnInit(): void {
   }
--- src/app/layout/main-layout/main-layout.service.ts
+++ src/app/layout/main-layout/main-layout.service.ts
@@ -12,7 +12,15 @@ export class MainLayoutService {
   showSidebar: BehaviorSubject<string> = new BehaviorSubject<string>('out');
   showSidebar$ = this.showSidebar.asObservable();
 
+  /**
+   * Indicates if the overlay is visible
+   */
+  showOverlay: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  showOverlay$ = this.showOverlay.asObservable();
+
   showHideSidebar(): void {
     this.showSidebar.getValue() === 'in' ? this.showSidebar.next('out') : this.showSidebar.next('in');
+    this.showOverlay.getValue() ? this.showOverlay.next(false) : this.showOverlay.next(true);
+
   }
 }
