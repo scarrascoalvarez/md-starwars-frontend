bbf98b4bee658a50cf295c30cd497331588789d6
Sergio Carrasco Álvarez <contact@sergiocarrasco.es>
2020-08-29 00:12:00 +0200

    Environment variables for production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  API_URL: 'https://md-starwars-backend.herokuapp.com'
+  API_AUTHENTICATION_URL: 'https://md-starwars-backend.herokuapp.com',
+  API_STARWARS_URL: 'https://swapi.dev/api',
+  CACHE_EXPIRATION_TIME: 300000,
+  CACHE_SIZE: 1,
+  API_IMAGE_URL: 'https://starwars-visualguide.com/assets/img/starships/'
 };
