4ac39d3ee29ad31b9bb6ff6a8e6cb8cc9913a288
Sergio Carrasco Álvarez <contact@sergiocarrasco.es>
2020-08-29 22:46:02 +0200

    Improved usability of the authentication guard message

--- src/app/core/guards/admin/admin.guard.ts
+++ src/app/core/guards/admin/admin.guard.ts
@@ -1,32 +1,51 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmationDialogComponent } from 'src/app/shared/confirmation-dialog/confirmation-dialog.component';
+import { MainLayoutService } from 'src/app/layout/main-layout/main-layout.service';
+import { takeUntil } from 'rxjs/internal/operators/takeUntil';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AdminGuard implements CanActivate {
+export class AdminGuard implements CanActivate, OnDestroy {
+
+  /**
+   * Use to destroy and prevent memory leaks
+   */
+  private destroy$: Subject<void> = new Subject<void>();
 
   constructor(
     private authenticationService: AuthenticationService,
+    private mainLayoutService: MainLayoutService,
     public router: Router,
     public dialog: MatDialog
-    ) { }
+  ) { }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   canActivate(): boolean {
     if (this.authenticationService.isAdmin) {
       return true;
     } else {
       this.router.navigate([]);
-      this.dialog.open(ConfirmationDialogComponent, {
+      var dialogRef = this.dialog.open(ConfirmationDialogComponent, {
         width: '750px',
         data: {
           title: 'Solo usuarios administradores',
           description: 'Inicia sesión con un usuario administrador para poder ver esta página.'
         }
+      });
+      dialogRef.afterClosed()
+      .pipe(
+        takeUntil(this.destroy$)
+      ).subscribe(() => {
+        this.mainLayoutService.showHideSidebar();
       });
       return false;
     }
--- src/app/core/guards/authentication/authentication.guard.ts
+++ src/app/core/guards/authentication/authentication.guard.ts
@@ -1,35 +1,54 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmationDialogComponent } from 'src/app/shared/confirmation-dialog/confirmation-dialog.component';
+import { takeUntil } from 'rxjs/operators';
+import { MainLayoutService } from 'src/app/layout/main-layout/main-layout.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationGuard implements CanActivate {
+export class AuthenticationGuard implements CanActivate, OnDestroy {
+
+  /**
+   * Use to destroy and prevent memory leaks
+   */
+  private destroy$: Subject<void> = new Subject<void>();
 
   constructor(
     private authenticationService: AuthenticationService,
+    private mainLayoutService: MainLayoutService,
     public router: Router,
     public dialog: MatDialog
-    ) { }
+  ) { }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   canActivate(): boolean {
     if (this.authenticationService.isAuthenticated) {
       return true;
     } else {
       this.router.navigate([]);
-      this.dialog.open(ConfirmationDialogComponent, {
+      var dialogRef = this.dialog.open(ConfirmationDialogComponent, {
         width: '750px',
         data: {
           title: 'Solo usuarios registrados',
           description: 'Inicia sesión para poder ver esta página.'
         }
       });
+      dialogRef.afterClosed()
+        .pipe(
+          takeUntil(this.destroy$)
+        ).subscribe(() => {
+          this.mainLayoutService.showHideSidebar();
+        });
       return false;
     }
   }
-  
+
 }
