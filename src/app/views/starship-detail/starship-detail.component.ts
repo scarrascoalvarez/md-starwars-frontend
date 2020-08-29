4db0897f8e32e1d1f192ad5ec0806fc5e5adc6f5
Sergio Carrasco Álvarez <contact@sergiocarrasco.es>
2020-08-29 02:39:49 +0200

    Redirection to the detail of a ship and obtaining the id by parameter

--- src/app/views/starship-detail/starship-detail.component.ts
+++ src/app/views/starship-detail/starship-detail.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subject } from 'rxjs/internal/Subject';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-starship-detail',
   templateUrl: './starship-detail.component.html',
   styleUrls: ['./starship-detail.component.scss']
 })
-export class StarshipDetailComponent implements OnInit {
+export class StarshipDetailComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  /**
+   * Use to destroy and prevent memory leaks
+   */
+  private destroy$: Subject<void> = new Subject<void>();
+
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe((params: Params) => {
+      console.log(params);
+    })
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 }
